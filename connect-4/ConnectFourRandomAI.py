ed019fae7c9dc780cd7f71d7019c0e04ea8b23ce
Nathaniel B <30476356+NathanielB@users.noreply.github.com>
2018-09-23 16:32:09 +0100

    Revert "Here is my code and write up for project 1 of the CCA assessment"
    This reverts commit 709d495988dd664a62ca14572213b1253853cd2f.

--- connect-4/ConnectFourRandomAI.py
+++
@@ -1,5 +0,0 @@
-import random
-import ConnectFourBoard
-def AIcheck(board, token):  # randomly select a column
-    available_moves = board.not_full_columns()
-    return random.choice(available_moves)
